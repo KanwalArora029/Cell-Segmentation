fce3436c9bbb696cb1d6f6f9864738c04645c3a2
Kanwal Arora <kanwalarora029@gmail.com>
2023-06-28 20:00:09 +0100

    Segmentation Notebook

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '1.0.0',
+    author= 'Kanwal Arora',
+    author_email= 'kanwalarora029@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
